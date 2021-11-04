4cdf6a6289cdde4957013d6631617cf927365183
Artur Sulej <artur@shedul.com>
2021-11-04 17:55:57 +0100

    Consumer handles multiple partitions

--- lib/maurer/consumer.ex
+++ lib/maurer/consumer.ex
@@ -1,37 +1,51 @@
 defmodule Maurer.Consumer do
   alias Maurer.MessagesLog
 
-  def stream(topic) do
-    Stream.unfold(
-      {:next, nil},
-      fn
-        {status, item_id} ->
-          item_id = item_id || MessagesLog.get_head_id(topic)
-
-          with item_id when not is_nil(item_id) <- item_id,
-               %{next: next_item_id, value: value} <- MessagesLog.get_item(item_id, topic) do
-            next_fun(status, item_id, next_item_id, value)
-          else
-            nil -> {nil, {:next, nil}}
-          end
-      end
-    )
+  def stream(topics) do
+    topics
+    |> Enum.map(fn topic -> {:next, nil, topic} end)
+    |> Stream.unfold(fn prev_states -> next_step(prev_states) end)
+    |> Stream.flat_map(& &1)
     |> Stream.reject(&is_nil/1)
   end
 
-  defp next_fun(status, item_id, next_item_id, value) do
+  defp next_step(prev_states) do
+    results =
+      prev_states
+      |> Enum.map(fn {status, item_id, topic} ->
+        Task.async(fn -> next_item(status, item_id, topic) end)
+      end)
+      |> Enum.map(&Task.await/1)
+
+    values = Enum.map(results, fn {value, _} -> value end)
+    states = Enum.map(results, fn {_, state} -> state end)
+    {values, states}
+  end
+
+  defp next_item(status, item_id, topic) do
+    item_id = item_id || MessagesLog.get_head_id(topic)
+
+    with item_id when not is_nil(item_id) <- item_id,
+         %{next: next_item_id, value: value} <- MessagesLog.get_item(item_id, topic) do
+      build_state(status, item_id, next_item_id, value, topic)
+    else
+      nil -> {nil, {:next, nil, topic}}
+    end
+  end
+
+  defp build_state(status, item_id, next_item_id, value, topic) do
     case {status, next_item_id} do
       {:next, nil} ->
-        {value, {:tail, item_id}}
+        {value, {:tail, item_id, topic}}
 
       {:next, next_item_id} ->
-        {value, {:next, next_item_id}}
+        {value, {:next, next_item_id, topic}}
 
       {:tail, nil} ->
-        {nil, {:tail, item_id}}
+        {nil, {:tail, item_id, topic}}
 
       {:tail, next_item_id} ->
-        {nil, {:next, next_item_id}}
+        {nil, {:next, next_item_id, topic}}
     end
   end
 end
